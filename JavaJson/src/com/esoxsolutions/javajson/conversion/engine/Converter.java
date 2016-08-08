c28af457881aaa62cf1d71d14b4b8476236f3b28
Iede Snoek <iede.snoek@kvk.nl>
2016-08-08 10:02:44 +0200

    Added convert to array

--- JavaJson/src/com/esoxsolutions/javajson/conversion/engine/Converter.java
+++ JavaJson/src/com/esoxsolutions/javajson/conversion/engine/Converter.java
@@ -28,6 +28,32 @@ public class Converter {
 		
 	}
 	
+	public String[] ConvertToArray(Object o) throws Exception {
+
+		ArrayList<String> jsonElements=new ArrayList<>();	
+		if (o!=null) {
+			ArrayList<Field> fields=(ArrayList<Field>) getAllFields(new ArrayList<Field>(),o.getClass());
+			for (Field f:fields) {
+				f.setAccessible(true);
+				JsonSerializable attribute=(JsonSerializable)f.getAnnotation(JsonSerializable.class);
+				if (attribute==null) {
+					continue;
+				}
+				try {
+					String jsonElement=buildElement(attribute.JsonFieldName(),f,o);
+					jsonElements.add(jsonElement);
+				} catch (IllegalArgumentException | IllegalAccessException e) {
+					throw e;
+				}
+				
+			}
+			
+		}
+		
+		return jsonElements.toArray(new String[jsonElements.size()]);
+
+	}
+	
 	public String Convert(Object o) throws Exception {
 		
 		if (o!=null) {
